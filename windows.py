6a6e12af4dd44b532718793467ff14398faa4590
Fasih <fasihdagia2006@gmail.com>
2025-02-12 18:22:45 +0500

    imported neccessary modules and seted up database

---
+++ windows.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+import pymongo as pm
+
+#data base set up
+client = pm.MongoClient("mongodb://localhost:27017/")
+db = client["financial_records"]
+inventory = client['inventory']
+
+#temprory data storage
+sale_transaction = {}
+inventory_sale = {}
+
+purchase_transaction = {}
