fbe2e3a9e43cf64e7729152ba4471a898af3fb9a
Fasih <fasihdagia2006@gmail.com>
2025-02-12 23:17:13 +0500

    Created purchase invoice window

--- windows.py
+++ windows.py
@@ -77,4 +77,43 @@ def sale_invoice_window(root):
     load_transactions(table_sale,table_account_receivable,sale_transaction,inventory_sale)
 
 def purchase_invoice_window(root):
-    pass
+    global inventory_sale
+    global purchase_transaction
+    #accessing the particular collection
+    account = db['purchase_invoice']
+
+    #removing existing widgets
+    for widget in root.winfo_children():
+        widget.destroy()
+
+    #basic window dimensions
+    root.geometry("1500x800")
+    root.minsize(1500,700)
+    #window title
+    root.title(f"Purchase Invoice")
+
+    tk.Label(root,text=f"Purchase Invoice",font=("Helvetica", 18)).pack(pady=10)
+    #buttons to add,delete and update a transaction
+    button_frame = tk.Frame(root)
+    button_frame.pack(pady=10)
+
+    tk.Button(button_frame,text='Generate Invoice', width=15,command=lambda:generate_invoice(root,purchase_transaction,account,inventory_sale,"+","Purchase",purchase_invoice_window)).grid(row=0, column=2,padx=5)
+    tk.Button(button_frame, text="Save", width=15, command=lambda:save(purchase_transaction,account,inventory_sale)).grid(row=0, column=3,padx=5)
+    tk.Button(button_frame, text="Back", width=15, command=lambda:main_window(root)).grid(row=0, column=4,padx=5)
+    tk.Button(button_frame, text="Exit", width=15, command=root.quit).grid(row=0, column=5,padx=5)
+
+    #to display Cash transaction
+    display_frame = Frame()
+    display_frame.pack(pady=10)
+
+    tk.Label(root,text=f"Account Receivable:",font=("Helvetica", 16)).pack(pady=5,)
+    table_account_receivable = ttk.Treeview(root, columns=("S.NO", "Date","Invoice.NO","Account Receivable","Item","Quantity","Unit", "Description","Rate", "Amount","GST","GST Amount","Further Tax","Further Tax Amount","Total Amount","Balance"), show="headings")
+    table_account_receivable.pack(fill=tk.BOTH, pady=10)
+
+    tk.Label(root,text=f"Purchase:",font=("Helvetica", 16)).pack(pady=5)
+    table_purchase = ttk.Treeview(root, columns=("S.NO", "Date","Invoice.NO","Item","Quantity","Unit","Rate", "Amount","Remaining Stock"), show="headings")
+    table_purchase.pack(fill=tk.BOTH, pady=10)
+    
+    table(table_account_receivable,table_purchase)
+
+    load_transactions(table_purchase,table_account_receivable,purchase_transaction,inventory_sale
