887f516919a74c9ade5afa3bbd5b6e7e59f077b0
Fasih <fasihdagia2006@gmail.com>
2025-02-12 18:25:22 +0500

    created main menu window

--- windows.py
+++ windows.py
@@ -13,3 +13,28 @@ sale_transaction = {}
 inventory_sale = {}
 
 purchase_transaction = {}
+
+def main_window(root):
+
+    for widget in root.winfo_children():
+        widget.destroy()
+
+    root.geometry("450x200")
+    root.minsize(350,200)
+    root.maxsize(450,500)
+
+    root.title("Financial Records")
+
+    tk.Label(root,text="Main Menu",font=("Helvetica",20)).pack(padx=50,pady=5)
+
+    btn_frame = Frame()
+    btn_frame.pack(fill=X, padx=33, pady=10)
+
+    tk.Button(btn_frame,text="Sale Invoice", font=("Helvetica",10),width=20, command=lambda:sale_invoice_window(root)).grid(padx=10,pady=10,row=0,column=0)
+    tk.Button(btn_frame,text="Purchase Invoice", font=("Helvetica",10),width=20, command=lambda:purchase_invoice_window(root)).grid(padx=10,pady=10,row=0,column=1)
+
+def sale_invoice_window(root):
+    pass
+
+def purchase_invoice_window(root):
+    pass
