d350755d5121681437ec3f26a0a64b3a8182eb07
Fasih <fasihdagia2006@gmail.com>
2025-03-17 18:36:24 +0500

    created a function to generate bank payments

--- payment_functions.py
+++ payment_functions.py
@@ -101,6 +101,118 @@ def generate_cash_payments(root,window,payments_temp,payment):
         messagebox.showinfo("Success","Cash Payment Generated Succesfully!")
         window(root)
 
+def generate_bank_payments(root,window,payments_temp,payment,customers):
+    
+    for widget in root.winfo_children():
+        widget.destroy()
+
+    root.title("Generate Payment")
+
+    root.geometry("600x350")
+    root.minsize(600,350)
+
+    tk.Label(root,text="Generate Bank Payments",font=("helvetica",18,"bold")).pack(pady=30)
+
+    entry_frame = tk.Frame(root)
+    entry_frame.pack()
+
+    tk.Label(entry_frame, text="Date:", font=("helvetica",10)).grid(pady=10,row=0,column=0)
+    date_default = tk.StringVar(value=datetime.now().date())
+    date_entry = tk.Entry(entry_frame, width=20, textvariable=date_default)
+    date_entry.grid(row=0,column=1,padx=5)
+
+    tk.Label(entry_frame, text="Voucher No:", font=("helvetica",10)).grid(padx=5,pady=10,row=0,column=2)
+    voucher_no_entry = tk.Entry(entry_frame, width=20)
+    voucher_no_entry.grid(row=0,column=3)
+
+    tk.Label(entry_frame,text="Bank:",font=('helvetica',10)).grid(pady=10,row=1,column=0)
+    bank_options = ["Bank1","Bank2","Bank3"]
+    bank_option = tk.StringVar(value="Banks")
+    bank_entry = OptionMenu(entry_frame, bank_option , *bank_options)
+    bank_entry.config(width=19)
+    bank_entry.grid(row=1,column=1,padx=5)
+
+    tk.Label(entry_frame,text="Account Receivable:",font=('helvetica',9)).grid(pady=10,row=1,column=2)
+    acc_recev_options = []
+    for i in customers['customer_info'].find():
+            acc_recev_options.append(i.get('account_receivable',''))  
+    if len(acc_recev_options) == 0:
+        acc_recev_options.append("No Accounts to show")  
+    acc_recev_options.sort()      
+    acc_recev_option = tk.StringVar(value="Name")
+    acc_recev_entry = OptionMenu(entry_frame, acc_recev_option , *acc_recev_options)
+    acc_recev_entry.grid(row=1,column=1,pady=5,padx=5)
+    acc_recev_entry.config(width=15)
+    acc_recev_entry.grid(row=1,column=3,padx=5)
+
+    tk.Label(entry_frame, text="Expense Type:", font=("helvetica",10)).grid(pady=10,row=2,column=0)
+    exp_type_options = ["CONVEYANCE EXPENSE"]
+    exp_type_option = tk.StringVar(value="Expense Types")
+    exp_type_entry = OptionMenu(entry_frame, exp_type_option , *exp_type_options)
+    exp_type_entry.config(width=19)
+    exp_type_entry.grid(row=2,column=1,padx=5)
+
+    tk.Label(entry_frame, text="Description:", font=("helvetica",10)).grid(padx=5,pady=10,row=2,column=2)
+    description_entry = tk.Entry(entry_frame, width=20)
+    description_entry.grid(row=2,column=3)
+
+    tk.Label(entry_frame, text="Amount:", font=("helvetica",10)).grid(pady=10,row=3,column=0)
+    amount_entry = tk.Entry(entry_frame, width=20)
+    amount_entry.grid(row=3,column=1,padx=5)
+
+    tk.Button(root,text="Generate" ,font=("helvetica",10),width=20,command=lambda:generate(root,window,payments_temp,payment)).pack(pady=10)    
+    
+    btn_frame = tk.Frame(root) 
+    btn_frame.pack()
+
+    tk.Button(btn_frame,text="Back" ,font=("helvetica",10),width=10).grid(row=0,column=0,padx=5)
+    tk.Button(btn_frame,text="Exit" ,font=("helvetica",10),width=10,command=root.quit).grid(row=0,column=1,padx=5)
+
+    def generate(root,window,payments_temp,payment):
+        
+        date = date_entry.get()
+        voucher_no = voucher_no_entry.get()
+        bank = bank_option.get()
+        acc_recev =acc_recev_option.get()
+        exp_type = exp_type_option.get()
+        description = description_entry.get()
+        amount = int(amount_entry.get())
+
+        no_entries = payment.count_documents({})
+        if len(payments_temp)==0:
+            if no_entries == 0:
+                balance = 0 
+            else:
+                last_entry = payment.find_one(sort=[("_id", -1)])
+                balance = last_entry.get("balance","")
+        else: 
+            balance = payments_temp[len(payments_temp)]["balance"]
+
+        if len(payments_temp) == 0:
+            sno = no_entries + 1
+        else:
+            sno = no_entries + len(payments_temp) + 1
+
+        amountiw = num2words(amount, to='currency', lang='en_IN')
+        balance -= amount 
+
+        payments_temp[len(payments_temp)+1] = {
+
+            "s_no":sno,
+            "date":date,
+            "voucher_no":voucher_no,
+            "bank":bank,
+            "account_receviable":acc_recev,
+            "exp_type":exp_type,
+            "description":description,
+            "amount":amount,
+            "amountiw":amountiw,
+            "balance":balance
+        }
+
+        messagebox.showinfo("Success","Cash Payment Generated Succesfully!")
+        window(root)
+
 def load_payments(table_entry,payments_temp,pay_type):
     for row in table_entry.get_children():
         table_entry.delete(row)
--- windows.py
+++ windows.py
@@ -3,10 +3,10 @@ from tkinter import *
 from tkinter import ttk
 import pymongo as pm
 
-from functions import generate_contract, print_contracts, generate_invoice ,save, load_transactions ,table, back, return_invoice,print_invoice,table_contract,load_contracts,save_contract
+from functions import generate_contract,print_contracts,generate_invoice,save,load_transactions,table,back,return_invoice,print_invoice,table_contract,load_contracts,save_contract
 from inventory_functions import inventory_check,existing_products,add_product,remove_product
 from client_function import client_check,existing_clients,add_client,remove_client
-from payment_functions import go_back, generate_cash_payments, load_payments
+from payment_functions import go_back,generate_cash_payments,generate_bank_payments,load_payments
 
 #data base set up
 client = pm.MongoClient("mongodb://localhost:27017/")
@@ -746,7 +746,7 @@ def remove_client_window(root):
 
 def bank_payment_window(root):
     
-    account = payment['cash_payments']
+    account = payment['bank_payments']
 
     for widget in root.winfo_children():
         widget.destroy()
@@ -761,7 +761,7 @@ def bank_payment_window(root):
     btn_frame = tk.Frame()
     btn_frame.pack()
 
-    tk.Button(btn_frame,text="Generate Payment", font=("Helvetica",10),width=15, command=lambda:generate_cash_payments(root,cash_payment_window,payments_temp,account)).grid(padx=5,row=0,column=0)
+    tk.Button(btn_frame,text="Generate Payment", font=("Helvetica",10),width=15, command=lambda:generate_bank_payments(root,cash_payment_window,payments_temp,account,customers)).grid(padx=5,row=0,column=0)
     tk.Button(btn_frame,text="Save", font=("Helvetica",10),width=15).grid(padx=5,row=0,column=1)
     tk.Button(btn_frame,text="Back", font=("Helvetica",10),width=10,command=lambda:go_back(root,payment_module_window,payments_temp)).grid(padx=5,row=0,column=2)
     tk.Button(btn_frame,text="Exit", font=("Helvetica",10),width=10,command=root.quit).grid(padx=5,row=0,column=3)
