c9b5f1f9b1ecbc2cf5ccdce7bdc57594b59ba8ef
Fasih <fasihdagia2006@gmail.com>
2025-05-15 02:50:51 +0500

    created a function to show purchase ledger

--- ledger_functions.py
+++ ledger_functions.py
@@ -34,3 +34,35 @@ def sale_show_account(acc_name,customers,table_ledger):
                 ))
         j += 1
 
+def purchase_show_account(acc_name,customers,table_ledger):
+    account_name = acc_name.get()
+    account = customers[f"purchase_invoice_{account_name}"]
+
+    for entry in table_ledger.get_children():
+        table_ledger.delete(entry)
+
+    j = 1
+    for entry in account.find():
+        if entry.get("invoice_type") != None:    
+            table_ledger.insert("", tk.END, values=(
+                j,
+                entry.get('date', ''),
+                entry.get('voucher_no', '0'),
+                entry.get('head_type','Nill'),
+                entry.get('Description', ''),
+                entry.get('', '0'),
+                entry.get('amount', ''),
+                entry.get('balance',''),        
+                ))
+        else:
+            table_ledger.insert("", tk.END, values=(
+                j,
+                entry.get('date', ''),
+                entry.get('voucher_no', '0'),
+                entry.get('head_type','Nill'),
+                entry.get('Description', ''),
+                entry.get('amount', ''),
+                entry.get('', '0'),
+                entry.get('balance',''),        
+                ))
+        j += 1
