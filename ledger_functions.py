8da3fbab6ee4fe7eb1fb49dadcdd0b600c498995
Fasih <fasihdagia2006@gmail.com>
2025-05-18 18:46:31 +0500

    implemeted the date range in sale ledger

--- ledger_functions.py
+++ ledger_functions.py
@@ -1,41 +1,55 @@
 import tkinter as tk
 from tkinter import *
+from tkinter import messagebox
 
-def sale_show_account(acc_name,customers,table_ledger):
+def sale_show_account(acc_name,customers,table_ledger,from_entry,to_entry):
     account_name = acc_name.get()
+    from_date = from_entry.get()
+    to_date = to_entry.get()
     account = customers[f"sale_invoice_{account_name}"]
 
     for entry in table_ledger.get_children():
         table_ledger.delete(entry)
 
-    j = 1
-    for entry in account.find():
-        if entry.get("invoice_type") != None:    
-            table_ledger.insert("", tk.END, values=(
-                j,
-                entry.get('date', ''),
-                entry.get('invoice_no', '0'),
-                entry.get('head_type','Nill'),
-                entry.get('Description', ''),
-                entry.get('', '0'),
-                entry.get('amount', ''),
-                entry.get('balance',''),        
-                ))
-        else:
-            table_ledger.insert("", tk.END, values=(
-                j,
-                entry.get('date', ''),
-                entry.get('invoice_no', '0'),
-                entry.get('head_type','Nill'),
-                entry.get('Description', ''),
-                entry.get('amount', ''),
-                entry.get('', '0'),
-                entry.get('balance',''),        
-                ))
-        j += 1
+    entries = account.find({"date": {
+                "$gte": from_date,
+                "$lte": to_date}})
+    
+    if entries == None:
+        messagebox.showinfo("No Entries", "No entries found for the selected date range.")
+        return
 
-def purchase_show_account(acc_name,customers,table_ledger):
+    else:
+        j = 1
+        for entry in entries:
+            if entry.get("invoice_type") != None:    
+                table_ledger.insert("", tk.END, values=(
+                    j,
+                    entry.get('date', ''),
+                    entry.get('invoice_no', '0'),
+                    entry.get('head_type','Nill'),
+                    entry.get('Description', ''),
+                    entry.get('', '0'),
+                    entry.get('amount', ''),
+                    entry.get('balance',''),        
+                    ))
+            else:
+                table_ledger.insert("", tk.END, values=(
+                    j,
+                    entry.get('date', ''),
+                    entry.get('invoice_no', '0'),
+                    entry.get('head_type','Nill'),
+                    entry.get('Description', ''),
+                    entry.get('amount', ''),
+                    entry.get('', '0'),
+                    entry.get('balance',''),        
+                    ))
+            j += 1
+
+def purchase_show_account(acc_name,customers,table_ledger,from_entry,to_entry):
     account_name = acc_name.get()
+    from_date = from_entry.get()
+    to_date = to_entry.get()
     account = customers[f"purchase_invoice_{account_name}"]
 
     for entry in table_ledger.get_children():
