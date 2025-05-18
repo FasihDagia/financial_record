809ad547af90f94e6203d51c3e6fa13811e5c951
Fasih <fasihdagia2006@gmail.com>
2025-05-19 02:46:01 +0500

    fixed a bug of sale ledger

--- ledger_functions.py
+++ ledger_functions.py
@@ -26,9 +26,10 @@ def sale_show_account(acc_name,customers,table_ledger,from_entry,to_entry,bal_la
         messagebox.showinfo("No Entries", "No entries found for the selected date range.")
         return
     else:
-        if from_date != "":
-            range_start = account.find_one({"date":from_date})
-            sno = range_start.get("s_no")
+        if from_date != "" and count !=0:       
+            range_start = account.find(filt).sort("date", 1).limit(1)
+            range_first = next(range_start,None)
+            sno = range_first.get("s_no")
             for_bal = account.find_one({"s_no":sno-1})
             if for_bal != None:
                 balance = for_bal.get("balance")
@@ -36,6 +37,7 @@ def sale_show_account(acc_name,customers,table_ledger,from_entry,to_entry,bal_la
                 balance = 0
         else:
             balance = 0
+
         opening_balance =  f"{balance:.2f}"
         bal_label.config(text="Opening Balance:",pady=10,padx=10)
         bal_amount.config(text=opening_balance,pady=10,padx=10)
