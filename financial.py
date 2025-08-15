050d546048a1a2dac27c4297cee4a75b13cfff31
Fasih <fasihdagia2006@gmail.com>
2025-08-16 03:14:22 +0500

    created adjustment window

--- financial.py
+++ financial.py
@@ -15,25 +15,30 @@ def center_window(root, width, height):
     root.maxsize(width, height)
 
 def create_adjustment_window(root,adjustments,adjustment_temp,heads,window,company_name,user_name,customers):
+
+    style = ttk.Style()
+    style.configure("Module.TButton", font=("Helvetica", 11),borderwidth=4,padding=5)
+    style.configure("Logout.TButton", font=("Helvetica", 9),borderwidth=4,padding=2)
+    style.configure("Unit.TMenubutton",background="#ffffff",foreground="black", arrowcolor="black")
     
     for widget in root.winfo_children():
         widget.destroy()
 
-    center_window(root, 650, 450)
+    center_window(root, 650, 380)
 
     root.title("Financial Adjustment")
 
-    tk.Label(root, text="Generate Adjustment",font=("helvetica",18,"bold")).pack(pady=18)
+    ttk.Label(root, text="Generate Adjustment",font=("helvetica",18,"bold")).pack(pady=18)
 
     entry_frame = tk.Frame(root)
     entry_frame.pack()
 
-    tk.Label(entry_frame, text="Date:", font=("helvetica",10)).grid(pady=10,row=0,column=0)
+    ttk.Label(entry_frame, text="Date:", font=("helvetica",10)).grid(pady=10,row=0,column=0)
     date_default = tk.StringVar(value=datetime.now().date())
-    date_entry = tk.Entry(entry_frame, width=25, textvariable=date_default)
+    date_entry = ttk.Entry(entry_frame, width=25, textvariable=date_default)
     date_entry.grid(row=0,column=1,padx=5)
 
-    tk.Label(entry_frame, text="Voucher No:", font=("helvetica",11)).grid(padx=5,pady=10,row=0,column=2)
+    ttk.Label(entry_frame, text="Voucher No:", font=("helvetica",11)).grid(padx=5,pady=10,row=0,column=2)
     no_adj = adjustments.count_documents({})
     if len(adjustment_temp) == 0:
         voucher_no = no_adj+1
@@ -43,37 +48,48 @@ def create_adjustment_window(root,adjustments,adjustment_temp,heads,window,compa
     current_date = datetime.now()
     year = current_date.year
     voucher = f"JV{str(voucher_no).zfill(5)}/{year}"
+    ttk.Label(entry_frame,text=voucher,font=("Helvetica", 12)).grid(row=0,column=3)
 
-    tk.Label(entry_frame,text=voucher,font=("Helvetica", 12)).grid(row=0,column=3)
-     
-    tk.Label(entry_frame,text="Debit Transaction Mode:",font=("helvetica",10)).grid(pady=10,row=1,column=0) 
-    trans_mode_options = ["Bank Receipt","Bank Payment", "Cash Receipt", "Cash Payment"]
-    db_trans_option = tk.StringVar(value="Transaction Mode")
-    trans_mode_entry = OptionMenu(entry_frame, db_trans_option , *trans_mode_options)
-    trans_mode_entry.config(width=19)
-    trans_mode_entry.grid(row=1,column=1,padx=5)
+    def db_cr_acc_name(*args):
+        if db_exp_type_option.get() == "Payment" or db_exp_type_option.get() == "Receipt":
+            center_window(root, 650, 430)
+            db_acc_label.grid(pady=10,row=2,column=0)
+            db_combo.grid(row=2, column=1, pady=5)
+        elif db_exp_type_option.get() != "Payment" or db_exp_type_option.get() != "Receipt":
+            db_acc_label.grid_remove()
+            db_combo.grid_remove()
+            if not cr_acc_label.winfo_ismapped() and not cr_combo.winfo_ismapped():
+                center_window(root, 650, 380)
 
-    tk.Label(entry_frame,text="Credit Transaction Mode:",font=("helvetica",10)).grid(pady=10,row=1,column=2) 
-    cr_trans_option = tk.StringVar(value="Transaction Mode")
-    trans_mode_entry = OptionMenu(entry_frame, cr_trans_option , *trans_mode_options)
-    trans_mode_entry.config(width=19)
-    trans_mode_entry.grid(row=1,column=3,padx=5)
 
-    tk.Label(entry_frame, text="Debit Head Type:", font=("helvetica",10)).grid(pady=10,row=2,column=0)
-    exp_type_options = []
+        if cr_exp_type_option.get() == "Payment" or cr_exp_type_option.get() == "Receipt":
+            center_window(root, 650, 430)
+            cr_acc_label.grid(pady=10,row=2,column=2)
+            cr_combo.grid(row=2, column=3, pady=5)
+        elif cr_exp_type_option.get() != "Payment" or cr_exp_type_option.get() != "Receipt":
+            cr_acc_label.grid_remove()
+            cr_combo.grid_remove()
+            if not db_acc_label.winfo_ismapped()  and not db_combo.winfo_ismapped():
+                center_window(root, 650, 380)
+    
+    exp_type_options = ["Head Types","Payment","Receipt","Tax"]
     for i in heads.find():
         exp_type_options.append(i.get('hd_name',''))
-    exp_type_options.sort()
+     
+    ttk.Label(entry_frame, text="Debit Head Type:", font=("helvetica",10)).grid(pady=10,row=1,column=0)
     db_exp_type_option = tk.StringVar(value="Head Types")
-    exp_type_entry = OptionMenu(entry_frame, db_exp_type_option , *exp_type_options)
-    exp_type_entry.config(width=19)
-    exp_type_entry.grid(row=2,column=1,padx=5)
+    exp_type_entry = ttk.OptionMenu(entry_frame, db_exp_type_option , *exp_type_options)
+    exp_type_entry.config(width=19, style="Unit.TMenubutton")
+    exp_type_entry.grid(row=1,column=1,padx=5)
 
-    tk.Label(entry_frame, text="Credit Head Type:", font=("helvetica",10)).grid(pady=10,row=2,column=2)
+    ttk.Label(entry_frame, text="Credit Head Type:", font=("helvetica",10)).grid(pady=10,row=1,column=2)
     cr_exp_type_option = tk.StringVar(value="Head Types")
-    exp_type_entry = OptionMenu(entry_frame, cr_exp_type_option , *exp_type_options)
-    exp_type_entry.config(width=19)
-    exp_type_entry.grid(row=2,column=3,padx=5)
+    exp_type_entry = ttk.OptionMenu(entry_frame, cr_exp_type_option , *exp_type_options)
+    exp_type_entry.config(width=19,style="Unit.TMenubutton")
+    exp_type_entry.grid(row=1,column=3,padx=5)
+
+    db_exp_type_option.trace_add("write",db_cr_acc_name)
+    cr_exp_type_option.trace_add("write",db_cr_acc_name)
 
     account_name = []
     for name in customers["customer_info"].find():
@@ -81,35 +97,32 @@ def create_adjustment_window(root,adjustments,adjustment_temp,heads,window,compa
     if len(account_name)==0:
         account_name.append("No Accounts Found")
     account_name.sort()
-    db_acc_label = tk.Label(entry_frame, text="Debit Account Name:", font=("helvetica",10))
-    db_acc_label.grid(pady=10,row=3,column=0)
+    db_acc_label = ttk.Label(entry_frame, text="Debit Account Name:", font=("helvetica",10))
     db_selected_account = tk.StringVar(value="Select Account")
     db_combo = AutocompleteCombobox(entry_frame, textvariable=db_selected_account, width=20, font=("Helvetica", 10))
     db_combo.set_completion_list(account_name)  
-    db_combo.grid(row=3, column=1, pady=5)
+    
 
-    cr_acc_label =tk.Label(entry_frame, text="Credit Account Name:", font=("helvetica",10))
-    cr_acc_label.grid(pady=10,row=3,column=2)
+    cr_acc_label =ttk.Label(entry_frame, text="Credit Account Name:", font=("helvetica",10))
     cr_selected_account = tk.StringVar(value="Select Account")
     cr_combo = AutocompleteCombobox(entry_frame, textvariable=cr_selected_account, width=20, font=("Helvetica", 10))
     cr_combo.set_completion_list(account_name)
-    cr_combo.grid(row=3, column=3, pady=5)
     
-    tk.Label(entry_frame, text="Amount:", font=("helvetica",10)).grid(pady=10,row=4,column=0)
-    amount_entry = tk.Entry(entry_frame, width=25)
-    amount_entry.grid(row=4,column=1,padx=5)
+    ttk.Label(entry_frame, text="Amount:", font=("helvetica",10)).grid(pady=10,row=3,column=0)
+    amount_entry = ttk.Entry(entry_frame, width=25)
+    amount_entry.grid(row=3,column=1,padx=5)
 
-    tk.Label(entry_frame, text="Remarks:", font=("helvetica",10)).grid(padx=5,pady=10,row=5,column=0)
+    ttk.Label(entry_frame, text="Remarks:", font=("helvetica",10)).grid(padx=5,pady=10,row=4,column=0)
     description_entry = tk.Text(entry_frame,font=("helvetica",10),width=50,height=5)
-    description_entry.grid(row=5,column=1,columnspan=3,padx=5,)
+    description_entry.grid(row=4,column=1,columnspan=3,padx=5,)
 
-    tk.Button(root,text="Generate" ,font=("helvetica",10),width=20,command=lambda:generate()).pack(pady=10)    
+    ttk.Button(root,text="Generate" ,style="Module.TButton",width=15,cursor="hand2",command=lambda:generate()).pack(pady=10)    
     
     btn_frame = tk.Frame(root) 
     btn_frame.pack()
 
-    tk.Button(btn_frame,text="Back" ,font=("helvetica",10),width=10,command=lambda:window(root,company_name,user_name)).grid(row=0,column=0,padx=5)
-    tk.Button(btn_frame,text="Exit" ,font=("helvetica",10),width=10,command=root.destroy).grid(row=0,column=1,padx=5)
+    ttk.Button(btn_frame,text="Back" ,style="Logout.TButton",cursor="hand2",width=10,command=lambda:window(root,company_name,user_name)).grid(row=0,column=0,padx=5)
+    ttk.Button(btn_frame,text="Exit" ,style="Logout.TButton",cursor="hand2",width=10,command=root.destroy).grid(row=0,column=1,padx=5)
 
     def generate():
         pass
