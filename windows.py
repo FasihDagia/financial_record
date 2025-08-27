879a1bc7c4432aaddf2884cb1eecabbbf8e9efe0
Fasih <fasihdagia2006@gmail.com>
2025-08-28 00:30:31 +0500

    created a feature to reset fields of the window

--- windows.py
+++ windows.py
@@ -1325,7 +1325,6 @@ def purchase_ledger_window(root,company_name,user_name):
 def adjustment_window(root,company_name,user_name):
 
     adjustments = db['adjustments']
-    print("adjustments:", adjustments)
     heads = client[f'company_profile_{company_name.lower().replace(" ", "_")}']['heads']
     bank_acc = client["banks"]
 
